40d81b8500ea82747052baf5ba9043a52071558f
Altayeb <altayebrabh@gmail.com>
2022-02-04 17:54:38 +0200

    save

--- resources/views/layouts/modals/students-list-modal.blade.php
+++ resources/views/layouts/modals/students-list-modal.blade.php
@@ -14,7 +14,11 @@
                             <label for="section">القسم</label>
                             <select name="section_id" id="section" class="form-control">
                                 <option></option>
-                                f
+                                @if(cache('sections') != null)
+                                    @foreach (cache('sections') as $section)
+                                        <option value="{{ $section->id }}">{{ $section->name }}</option>
+                                    @endforeach
+                                @endif
                             </select>
                         </div>
                         <div class="col-md-6 mb-3">
