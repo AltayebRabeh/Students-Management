a843dc7e4e876fa23e64a437faa3a6536693a501
Altayeb <altayebrabh@gmail.com>
2022-02-04 18:23:37 +0200

    save

---
+++ storage/framework/views/11514afaeb7bc444e1568dc753ce9f84dbb8f828.php
@@ -0,0 +1,40 @@
+<div class="modal fade" id="studentListModal" tabindex="-1" role="dialog" aria-labelledby="varyModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <form action="<?php echo e(route('students.list')); ?>" method="GET">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="varyModalLabel">عرض قائمة الطلاب</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <div class="row">
+                        <div class="col-md-6 mb-3">
+                            <label for="section">القسم</label>
+                            <select name="section_id" id="section" class="form-control">
+                                <option></option>
+                                <?php if(cache('sections') != null): ?>
+                                    <?php $__currentLoopData = cache('sections'); $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $section): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                                        <option value="<?php echo e($section->id); ?>"><?php echo e($section->name); ?></option>
+                                    <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                                <?php endif; ?>
+                            </select>
+                        </div>
+                        <div class="col-md-6 mb-3">
+                            <label for="classroom">الصف الدراسي</label>
+                            <select name="classroom_id" id="classroom" class="form-control">
+
+                            </select>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn mb-2 btn-secondary" data-dismiss="modal">إغلاق</button>
+                    <button type="submit" class="btn mb-2 btn-success">متابعة</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+<?php /**PATH /home/altayeb/Desktop/university/resources/views/layouts/modals/students-list-modal.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/5f6341ce5b0b11f951b4e3508184e3775560b11b.php
@@ -0,0 +1,262 @@
+<aside class="sidebar-left border-right bg-white shadow" id="leftSidebar" data-simplebar>
+    <a href="#" class="btn collapseSidebar toggle-btn d-lg-none text-muted ml-2 mt-3" data-toggle="toggle">
+      <i class="fe fe-x"><span class="sr-only"></span></i>
+    </a>
+    <nav class="vertnav navbar navbar-light">
+      <!-- nav bar -->
+      <div class="w-100 mb-4 d-flex">
+        <a class="navbar-brand mx-auto mt-2 flex-fill text-center" href="./index.html">
+            <img src="<?php echo e(cache('settings') != null ? asset('uploads/'.cache('settings')['logo']) : ''); ?>" alt="Rabeh" width="50px">
+        </a>
+      </div>
+      <ul class="navbar-nav flex-fill w-100 mb-2">
+        <li class="nav-item">
+          <a href="<?php echo e(route('dashboard')); ?>" class="nav-link">
+            <i class="fe fe-home fe-16"></i>
+            <span class="ml-3 item-text">لوحة التحكم</span>
+          </a>
+        </li>
+        <li class="nav-item dropdown">
+          <a href="#Students" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle nav-link">
+            <i class="fe fe-box fe-16"></i>
+            <span class="ml-3 item-text">الطلاب</span>
+          </a>
+          <ul class="collapse list-unstyled pl-4 w-100" id="Students">
+            <li class="nav-item">
+                <a class="nav-link pl-3" href="<?php echo e(route('students.index')); ?>"><span class="ml-1 item-text">عرض الطلاب</span>
+                </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link pl-3" href="<?php echo e(route('students.create')); ?>"><span class="ml-1 item-text">إضافة طلاب</span>
+              </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link pl-3" href="<?php echo e(route('archive.index')); ?>"><span class="ml-1 item-text">الارشيف</span>
+                </a>
+              </li>
+          </ul>
+        </li>
+        <li class="nav-item dropdown">
+            <a href="#lists" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle nav-link">
+              <i class="fe fe-list fe-16"></i>
+              <span class="ml-3 item-text">القوائم</span>
+            </a>
+            <ul class="collapse list-unstyled pl-4 w-100" id="lists">
+              <li class="nav-item">
+                  <a class="nav-link pl-3" data-toggle="modal" data-target="#studentListModal" href="javascript:void();"><span class="ml-1 item-text">القوائم الطلاب</span>
+                  </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link pl-3" data-toggle="modal" data-target="#supplementListModal" href="javascript:void();"><span class="ml-1 item-text">القوائم الطلاب للملاحق والبدائل</span>
+                </a>
+            </li>
+            </ul>
+        </li>
+        <li class="nav-item dropdown">
+            <a href="#exams" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle nav-link">
+              <i class="fe fe-bookmark fe-16"></i>
+              <span class="ml-3 item-text">النتائج</span>
+            </a>
+            <ul class="collapse list-unstyled pl-4 w-100" id="exams">
+              <li class="nav-item">
+                  <a class="nav-link pl-3" href="<?php echo e(route('grades.index')); ?>"><span class="ml-1 item-text">ادخال نتيجة فصل</span>
+                  </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link pl-3" href="<?php echo e(route('student-grades.index')); ?>"><span class="ml-1 item-text">ادخال نتيجة طالب</span>
+                </a>
+                </li>
+              <li class="nav-item">
+                <a class="nav-link pl-3" href="<?php echo e(route('grades.semester.data')); ?>"><span class="ml-1 item-text">عرض نتيجة فصل</span>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link pl-3" data-toggle="modal" data-target="#studentResultModal" href="javascript:void();"><span class="ml-1 item-text">عرض نتيجة طالب</span>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link pl-3" href="<?php echo e(route('student-grades.semester-data')); ?>"><span class="ml-1 item-text">عرض نتيجة فصل لطالب</span>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link pl-3" href="<?php echo e(route('grades.delete')); ?>"><span class="ml-1 item-text">حذف نتيجة مادة لفصل</span>
+                </a>
+              </li>
+            </ul>
+        </li>
+        <li class="nav-item dropdown">
+            <a href="#supplements" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle nav-link">
+              <i class="fe fe-box fe-16"></i>
+              <span class="ml-3 item-text">الملاحق والبدائل</span>
+            </a>
+            <ul class="collapse list-unstyled pl-4 w-100" id="supplements">
+              <li class="nav-item">
+                  <a class="nav-link pl-3" href="<?php echo e(route('supplements.index')); ?>"><span class="ml-1 item-text">ادخال نتيجة</span>
+                  </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link pl-3" href="<?php echo e(route('supplements.data')); ?>"><span class="ml-1 item-text">عرض نتيجة</span>
+                </a>
+              </li>
+            </ul>
+        </li>
+        <li class="nav-item dropdown">
+            <a href="#sections" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle nav-link">
+              <i class="fe fe-folder fe-16"></i>
+              <span class="ml-3 item-text">الاقسام</span>
+            </a>
+            <ul class="collapse list-unstyled pl-4 w-100" id="sections">
+              <li class="nav-item">
+                  <a class="nav-link pl-3" href="<?php echo e(route('sections.index')); ?>"><span class="ml-1 item-text">عرض الاقسام</span>
+                  </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link pl-3" href="<?php echo e(route('sections.create')); ?>"><span class="ml-1 item-text">إضافة قسم</span>
+                </a>
+              </li>
+            </ul>
+          </li>
+        <li class="nav-item dropdown">
+            <a href="#subjects" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle nav-link">
+              <i class="fe fe-book-open fe-16"></i>
+              <span class="ml-3 item-text">المواد</span>
+            </a>
+            <ul class="collapse list-unstyled pl-4 w-100" id="subjects">
+              <li class="nav-item">
+                <a class="nav-link pl-3" href="<?php echo e(route('subjects.index')); ?>"><span class="ml-1 item-text">عرض المواد</span>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link pl-3" href="<?php echo e(route('subjects.create')); ?>"><span class="ml-1 item-text">إضافة مادة</span>
+                </a>
+              </li>
+            </ul>
+        </li>
+        <li class="nav-item dropdown">
+            <a href="#teachers" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle nav-link">
+              <i class="fe fe-briefcase fe-16"></i>
+              <span class="ml-3 item-text">الاساتذة</span>
+            </a>
+            <ul class="collapse list-unstyled pl-4 w-100" id="teachers">
+              <li class="nav-item">
+                <a class="nav-link pl-3" href="<?php echo e(route('teachers.index')); ?>"><span class="ml-1 item-text">عرض الاساتذة</span>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link pl-3" href="<?php echo e(route('teachers.create')); ?>"><span class="ml-1 item-text">إضافة استاذ</span>
+                </a>
+              </li>
+            </ul>
+        </li>
+        <li class="nav-item dropdown">
+            <a href="#subjectsTeachers" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle nav-link">
+              <i class="fe fe-link fe-16"></i>
+              <span class="ml-3 item-text">المواد والاساتذة</span>
+            </a>
+            <ul class="collapse list-unstyled pl-4 w-100" id="subjectsTeachers">
+              <li class="nav-item">
+                <a class="nav-link pl-3" href="<?php echo e(route('subjects-teachers.index')); ?>"><span class="ml-1 item-text">عرض المواد والاساتذة</span>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link pl-3" href="<?php echo e(route('subjects-teachers.create')); ?>"><span class="ml-1 item-text">دمج</span>
+                </a>
+              </li>
+            </ul>
+        </li>
+
+        <li class="nav-item dropdown">
+            <a href="#equations" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle nav-link">
+              <i class="fe fe-plus-square fe-16"></i>
+              <span class="ml-3 item-text">طريقة الحساب</span>
+            </a>
+            <ul class="collapse list-unstyled pl-4 w-100" id="equations">
+              <li class="nav-item">
+                <a class="nav-link pl-3" href="<?php echo e(route('equations.index')); ?>"><span class="ml-1 item-text">عرض الكل</span>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link pl-3" href="<?php echo e(route('equations.create')); ?>"><span class="ml-1 item-text">إضافة</span>
+                </a>
+              </li>
+            </ul>
+        </li>
+
+        <li class="nav-item dropdown">
+            <a href="#marks" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle nav-link">
+              <i class="fe fe-flag fe-16"></i>
+              <span class="ml-3 item-text">العلامات</span>
+            </a>
+            <ul class="collapse list-unstyled pl-4 w-100" id="marks">
+              <li class="nav-item">
+                <a class="nav-link pl-3" href="<?php echo e(route('marks.index')); ?>"><span class="ml-1 item-text">عرض الكل</span>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link pl-3" href="<?php echo e(route('marks.create')); ?>"><span class="ml-1 item-text">إضافة</span>
+                </a>
+              </li>
+            </ul>
+        </li>
+
+        <li class="nav-item dropdown">
+            <a href="#years" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle nav-link">
+              <i class="fe fe-calendar fe-16"></i>
+              <span class="ml-3 item-text">السنين الدراسية</span>
+            </a>
+            <ul class="collapse list-unstyled pl-4 w-100" id="years">
+              <li class="nav-item">
+                <a class="nav-link pl-3" href="<?php echo e(route('years.index')); ?>"><span class="ml-1 item-text">عرض الكل</span>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link pl-3" href="<?php echo e(route('years.create')); ?>"><span class="ml-1 item-text">إضافة</span>
+                </a>
+              </li>
+            </ul>
+        </li>
+
+        <li class="nav-item dropdown">
+            <a href="#relays" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle nav-link">
+              <i class="fe fe-copy fe-16"></i>
+              <span class="ml-3 item-text">الترحيلات</span>
+            </a>
+            <ul class="collapse list-unstyled pl-4 w-100" id="relays">
+              <li class="nav-item">
+                <a class="nav-link pl-3" href="<?php echo e(route('relays.index')); ?>"><span class="ml-1 item-text">عرض الترحيلات</span>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link pl-3" href="<?php echo e(route('relays.create')); ?>"><span class="ml-1 item-text">ترحيل جديد</span>
+                </a>
+              </li>
+            </ul>
+        </li>
+
+        <li class="nav-item dropdown">
+            <a href="#users" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle nav-link">
+              <i class="fe fe-user fe-16"></i>
+              <span class="ml-3 item-text">المستخدمين</span>
+            </a>
+            <ul class="collapse list-unstyled pl-4 w-100" id="users">
+              <li class="nav-item">
+                <a class="nav-link pl-3" href="./ui-color.html"><span class="ml-1 item-text">عرض المستخدمين</span>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link pl-3" href="./ui-color.html"><span class="ml-1 item-text">إضافة مستخدم</span>
+                </a>
+              </li>
+            </ul>
+        </li>
+
+        <li class="nav-item">
+            <a href="<?php echo e(route('settings.index')); ?>" class="nav-link">
+              <i class="fe fe-settings fe-16"></i>
+              <span class="ml-3 item-text">الاعدادات</span>
+            </a>
+          </li>
+      </ul>
+    </nav>
+  </aside>
+<?php /**PATH /home/altayeb/Desktop/university/resources/views/layouts/aside.blade.php ENDPATH**/ ?>
